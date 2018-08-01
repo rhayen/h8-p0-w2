cb8bb3e88d7af10a4bc76aa0b17646eead2608c5
rhayen <rhayen21@gmail.com>
2018-08-01 08:49:48 +0700

    tugas week 2

---
+++ exercise-3.js
@@ -0,0 +1,26 @@
+// nama Nina peran Ksatria
+// nama Rhayen peran Tabib
+// nama Reva peran Penyihir
+
+
+var nama = "nina"
+var peran = "ksatria"
+
+if (nama == "nina" && peran == "ksatria")
+  {console.log ("Selamat Datang di Dunia Proxytia", nama)
+  console.log ("Halo", peran , nama , ",kamu dapat menyerang dengan senjatamu ")}
+
+
+
+if (nama == "rhayen" && peran == "tabib")
+  {console.log ("selamat datang di dunia proxytia", nama)
+  console.log ("Halo", peran , nama , ",kamu akan membantu temanmu yang terluka " )}
+
+
+
+if (nama == "reva" && peran == "penyihir")
+  {console.log ("selamat datang di dunia proxytia", nama)
+  console.log ("Halo", peran , nama , ",ciptakan keajaiban yang membantu kemenangan ")}
+else if (peran == "") {alert( "hello" + " "+nama+ " " + "Pilih Peranmu untuk memulai game")}
+
+else {console.log("")}
---
+++ exercise-4.js
@@ -0,0 +1,20 @@
+var tanggal =1
+var bulan =2
+var tahun =2010
+
+switch (bulan)
+{
+  case 1: {bulan = "januari"; break;}
+  case 2: {bulan = "februari"; break;}
+  case 3: {bulan = "maret"; break;}
+  case 4: {bulan = "april"; break;}
+  case 5: {bulan = "mei"; break;}
+  case 6: {bulan = "juni"; break;}
+  case 7: {bulan = "juli"; break;}
+  case 8: {bulan = "agustus"; break;}
+  case 9: {bulan = "september"; break;}
+  case 10: {bulan = "oktober"; break;}
+  case 11: {bulan = "november"; break;}
+  case 12: {bulan = "desember"; break;}
+}
+console.log(tanggal+" "+bulan+" " +tahun)
